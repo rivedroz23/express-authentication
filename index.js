a562bd1cf25f987f23dc328dc59e01220b79e027
rivedroz23 <river.droz@gmail.com>
2019-06-28 12:30:57 -0700

    finished authentication

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+// Module allows use of session
 const session = require('express-session');
+
+// Imports passport local strategy 
 const passport = require('./config/passportConfig');
+// module for flash messages 
+const flash = require('connect-flash');
+const isLoggedIn = require('./middleware/isLoggedIn');
+const helmet = require('helmet');
+
+// This is only used by the session store
+const db = require('./models');
+
 const app = express();
+
+// This line makes the session use sequelize to write session data to a postgres table
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sessionStore = new SequelizeStore({
+  db: db.sequelize,
+  expiration: 1000 * 60 * 30
+});
 
 app.set('view engine', 'ejs');
 
@@ -11,23 +30,38 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(ejsLayouts);
+app.use(helmet());
 
 // Configures express-session middleware
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: true
-}));
+  saveUninitialized: true,
+  store: sessionStore
+})); 
+
+// Use this line once to set up the store table
+sessionStore.sync();
+
+// Starts the flash middleware
+app.use(flash());
 
 // Link passport to the express session
+// Must be below session
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(function(req, res, next) {
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+})
 
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/profile', function(req, res) {
+app.get('/profile', isLoggedIn, function(req, res) {
   res.render('profile');
 });
 
